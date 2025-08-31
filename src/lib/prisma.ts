a6b566f21d1ae5c75f8502e9fac6bcae198325f3
Andrew Ton <andrewtonster@gmail.com>
2025-08-31 11:45:06 -0700

    change prisma output

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@/generated/prisma"; // or "@prisma/client" if you're using default
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
